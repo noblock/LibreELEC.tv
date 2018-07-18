7148e65ab6ed2342cfcebf123dd0282a08c5fd5c
Paul Kocialkowski <paul.kocialkowski@bootlin.com>
2018-07-18 15:29:21 +0200

    libva: Allow non-x86 build
    Signed-off-by: Paul Kocialkowski <paul.kocialkowski@bootlin.com>

--- packages/multimedia/libva/package.mk
+++ packages/multimedia/libva/package.mk
@@ -5,7 +5,6 @@
 PKG_NAME="libva"
 PKG_VERSION="2.3.0"
 PKG_SHA256="8d95e65c4d84d0f82097581e163d3770694c600cbb040ebd827f2d375e004f4b"
-PKG_ARCH="x86_64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://01.org/linuxmedia"
 PKG_URL="https://github.com/intel/libva/archive/$PKG_VERSION.tar.gz"
